5227272f3b74ef32729781c625ebd692836b0362
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-11-16 11:11:22 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,6 +5,8 @@ printf "Checking for bash..."; if [ $(hash bash >& /dev/null; echo $?) -eq 0 ]; 
 printf "Checking for make..."; if [ $(hash make >& /dev/null; echo $?) -eq 0 ]; then echo yes; else printf "\e[1;31mno\e[0m\n"; fi
 printf "Checking for perl..."; if [ $(hash perl >& /dev/null; echo $?) -eq 0 ]; then echo yes; else printf "\e[1;31mno\e[0m\n"; fi
 printf "Creating Makefile..."
+if [ "$(cat Makefile | head -n7 | tail -n1)" =~ /all/ ]; then echo no need
+else
 echo -e \
 "all: warn man_install\n"\
 "warn:\n"\
@@ -20,5 +22,6 @@ echo -e \
 "man_install:\n"\
 "	sudo cp gksudo.1 /usr/share/man/man1/\n" >> Makefile
 echo done
+fi
 echo Please install any missing packages from above.
 echo Please run \`make\` then \`sudo make bash_install\` or \`sudo make perl_install\` to install.
